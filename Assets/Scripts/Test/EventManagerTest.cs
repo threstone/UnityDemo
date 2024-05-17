2d4a0b3142d7b31a1dc331603261a3f9b7aef737
积木 <790325011@qq.com>
2024-05-17 20:49:49 +0800

    1

--- Assets/Scripts/Test/EventManagerTest.cs
+++ Assets/Scripts/Test/EventManagerTest.cs
@@ -10,6 +10,11 @@ public class EventManagerTest : MonoBehaviour
 
     private void Test()
     {
-      
+
+        UnityTimer.Timer.Register(5f, () => Debug.Log("Hello World"));
+        UnityTimer.Timer.Register(3f, () =>
+        {
+            Debug.Log("123");
+        });
     }
 }
