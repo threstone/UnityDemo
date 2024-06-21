34e91901fb500d94aca69d02872e241fec9f0c57
积木 <790325011@qq.com>
2024-06-21 21:54:21 +0800

    重构

--- Assets/Scripts/Battle/Component/Buff/Buff.cs
+++ Assets/Scripts/Battle/Component/Buff/Buff.cs
@@ -28,4 +28,12 @@ public abstract class Buff : GameNode
     {
         Duration = Math.Max(duration, Duration);
     }
+
+    // 当状态自然结束
+    public void OnBuffAdd() { }
+    // 当状态自然结束
+    public void OnBuffEnd() { }
+
+    // 当Buff被驱散
+    public void OnBuffClear() { }
 }
--- Assets/Scripts/Battle/GameNode.cs
+++ Assets/Scripts/Battle/GameNode.cs
@@ -1,13 +1,5 @@
 public class GameNode
 {
-    // 当状态自然结束
-    public void OnBuffAdd() { }
-    // 当状态自然结束
-    public void OnBuffEnd() { }
-
-    // 当Buff被驱散
-    public void OnBuffClear() { }
-
     // 当被攻击前
     public void OnPreBeAttack(Damage damage) { }
 
@@ -17,14 +9,6 @@ public class GameNode
 
 public enum GameNodeEvent
 {
-    // 当状态自然结束
-    OnBuffAdd,
-    // 当状态自然结束
-    OnBuffEnd,
-
-    // 当Buff被驱散
-    OnBuffClear,
-
     // 当被攻击前
     OnPreBeAttack,
     // 当被攻击后
